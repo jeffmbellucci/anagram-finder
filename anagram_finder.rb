a25f38830b32d72762951f21241429b750395caa
Jeff <jeffmbellucci@gmail.com>
2023-11-12 12:48:03 -0800

    Only use one example that has 10+ anagrams

--- anagram_finder.rb
+++ anagram_finder.rb
@@ -11,9 +11,10 @@ end
 def solution_tester
   anagrams = Anagrams.new
   solved = anagrams.find('zoo') == [] &&
+  anagrams.find('stop').sort == ["spot", "tops", "opts", "post", "pots"].sort &&
   anagrams.find('star').sort == ["tsar", "tars", "arts", "rast", "rats"].sort &&
-  anagrams.find('stare').sort == ["strae", "stear", "taser", "tares", "tears", "teras", "aster", "arets", "rates", "resat", "reast", "earst"].sort &&
-  anagrams.find('alerts').sort == ["alters", "artels", "laster", "estral", "ratels", "talers", "tarsel", "salter", "slater", "staler", "stelar"].sort &&
+  anagrams.find('least').sort == ["leats", "slate", "setal", "salet", "stela", "steal", "stale", "teals", "tesla", "tales", "taels"].sort &&
+  anagrams.find('cinema').sort == ["iceman", "anemic"].sort &&
   anagrams.find('pester').sort == ["peters", "petres", "preset"].sort &&
   anagrams.find('present').sort == ["penster", "repents", "serpent"].sort
 
