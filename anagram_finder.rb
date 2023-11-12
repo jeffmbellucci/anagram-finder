88c6a2fd701267c719c63fac86fa39e3edf08cf6
Jeff <jeffmbellucci@gmail.com>
2023-11-12 04:19:58 -0800

    put words in order of length

--- anagram_finder.rb
+++ anagram_finder.rb
@@ -12,8 +12,8 @@ def solution_tester
   anagrams = Anagrams.new
   solved = anagrams.find('zoo') == [] &&
   anagrams.find('star').sort == ["tsar", "tars", "arts", "rast", "rats"].sort &&
-  anagrams.find('alerts').sort == ["alters", "artels", "laster", "estral", "ratels", "talers", "tarsel", "salter", "slater", "staler", "stelar"].sort &&
   anagrams.find('stare').sort == ["strae", "stear", "taser", "tares", "tears", "teras", "aster", "arets", "rates", "resat", "reast", "earst"].sort &&
+  anagrams.find('alerts').sort == ["alters", "artels", "laster", "estral", "ratels", "talers", "tarsel", "salter", "slater", "staler", "stelar"].sort &&
   anagrams.find('pester').sort == ["peters", "petres", "preset"].sort &&
   anagrams.find('present').sort == ["penster", "repents", "serpent"].sort
 
