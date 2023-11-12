6e63ea2b027d1688a331291873c29e86cd7acaee
Jeff <jeffmbellucci@gmail.com>
2023-11-11 19:26:34 -0800

    missing &&

--- anagram_finder.rb
+++ anagram_finder.rb
@@ -15,7 +15,7 @@ def solution_tester
   anagrams.find('alerts').sort == ["alters", "artels", "laster", "estral", "ratels", "talers", "tarsel", "salter", "slater", "staler", "stelar"].sort &&
   anagrams.find('pears').sort == ["pares", "parse", "presa", "prase", "apers", "apres", "asper", "reaps", "rapes", "spear", "spaer", "spare"].sort &&
   anagrams.find('stare').sort == ["strae", "stear", "taser", "tares", "tears", "teras", "aster", "arets", "rates", "resat", "reast", "earst"].sort &&
-  anagrams.find('star').sort == ["tsar", "tars", "arts", "rast", "rats"].sort
+  anagrams.find('star').sort == ["tsar", "tars", "arts", "rast", "rats"].sort  &&
   anagrams.find('pester').sort == ["peters", "petres", "preset"].sort
 
   solved ? "All tests passed!" : "Test(s) failed."
