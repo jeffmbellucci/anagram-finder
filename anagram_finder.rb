5f863409c02692f861a9046a80058cff59a21430
Jeff <jeffmbellucci@gmail.com>
2023-11-12 17:15:53 -0800

    Align &&s

--- anagram_finder.rb
+++ anagram_finder.rb
@@ -10,15 +10,15 @@ end
 
 def solution_tester
   anagrams = Anagrams.new
-  solved = anagrams.find('zoo') == [] &&
-    anagrams.find('stop').sort == %w[spot tops opts post pots].sort &&
-    anagrams.find('star').sort == %w[tsar tars arts rast rats].sort &&
-    anagrams.find('least').sort == %w[leats slate setal salet stela steal stale teals tesla tales taels].sort &&
-    anagrams.find('cinema').sort == %w[iceman anemic].sort &&
-    anagrams.find('pester').sort == %w[peters petres preset].sort &&
-    anagrams.find('present').sort == %w[penster repents serpent].sort
+  solved = anagrams.find('zoo') == []
+  solved &&= anagrams.find('stop').sort == %w[spot tops opts post pots].sort
+  solved &&= anagrams.find('star').sort == %w[tsar tars arts rast rats].sort
+  solved &&= anagrams.find('least').sort == %w[leats slate setal salet stela steal stale teals tesla tales taels].sort
+  solved &&= anagrams.find('cinema').sort == %w[iceman anemic].sort
+  solved &&= anagrams.find('pester').sort == %w[peters petres preset].sort
+  solved &&= anagrams.find('present').sort == %w[penster repents serpent].sort
 
-  solved ? 'All tests passed!' : 'Test(s) failed.'
+  puts solved ? 'All tests passed!' : 'Test(s) failed.'
 end
 
-puts solution_tester
+solution_tester
