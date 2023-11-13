16fa115b64a411e5a0dc728a3c7dfc771efd267b
Jeff <jeffmbellucci@gmail.com>
2023-11-12 16:50:58 -0800

    Use %w notation and indent multiple &&

--- anagram_finder.rb
+++ anagram_finder.rb
@@ -11,12 +11,12 @@ end
 def solution_tester
   anagrams = Anagrams.new
   solved = anagrams.find('zoo') == [] &&
-  anagrams.find('stop').sort == ["spot", "tops", "opts", "post", "pots"].sort &&
-  anagrams.find('star').sort == ["tsar", "tars", "arts", "rast", "rats"].sort &&
-  anagrams.find('least').sort == ["leats", "slate", "setal", "salet", "stela", "steal", "stale", "teals", "tesla", "tales", "taels"].sort &&
-  anagrams.find('cinema').sort == ["iceman", "anemic"].sort &&
-  anagrams.find('pester').sort == ["peters", "petres", "preset"].sort &&
-  anagrams.find('present').sort == ["penster", "repents", "serpent"].sort
+    anagrams.find('stop').sort == %w[spot tops opts post pots].sort &&
+    anagrams.find('star').sort == %w[tsar tars arts rast rats].sort &&
+    anagrams.find('least').sort == %w[leats slate setal salet stela steal stale teals tesla tales taels].sort &&
+    anagrams.find('cinema').sort == %w[iceman anemic].sort &&
+    anagrams.find('pester').sort == %w[peters petres preset].sort &&
+    anagrams.find('present').sort == %w[penster repents serpent].sort
 
   solved ? "All tests passed!" : "Test(s) failed."
 end
