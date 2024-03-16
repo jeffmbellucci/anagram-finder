9ae07865688f6f33cd530e005f2121992e50bea9
Jeff <jeffmbellucci@gmail.com>
2024-03-16 15:57:27 -0700

    I think this is an improvement

--- spelling_bee_solver.rb
+++ spelling_bee_solver.rb
@@ -1,43 +1,30 @@
 class WordFinder
-  attr_reader :letters, :dict, :dict_trie, :candidates, :word_length, :key_letter_or_word
+  attr_reader :letters, :dict, :candidates, :word_length, :key_letter_or_word, :start_letters, :end_letters
 
-  def initialize(options = {})
-    defaults = { letters: '',
-                 word_length: '',
-                 key_letter_or_word: '',
-                 start_letters: '',
-                 end_letters: ''}
-    opts = defaults.merge(options)
-    @letters = opts[:letters]
-    @word_length = opts[:word_length]
-    @key_letter_or_word = opts[:key_letter_or_word]
-
+  def initialize(letters:, word_length:, key_letter_or_word:, start_letters:, end_letters:)
     @dict = File.readlines('./scrabble_dictionary.txt').map(&:chomp).map(&:downcase)
-
-    full_dict_length = @dict.length
-    @word_length = opts[:word_length].is_a?(Integer) ? opts[:word_length] : letters.length
+    @letters = letters
+    @word_length = word_length
+    @key_letter_or_word = key_letter_or_word
+    @start_letters = start_letters
+    @end_letters = end_letters
 
     # puts "\nFull dictionary length: #{full_dict_length} words."
     # puts "Using '#{opts[:start_letters]}' to start and containing '#{opts[:key_letter_or_word]}' using only '#{letters.split('').join(',')}'."
 
-    # first filter the dictionary to only include words of the correct length
+  end
+
+  def find
     @dict.select! do |word| word.length == word_length &&
       # only select words that contain the letters
       word.chars.all? { |letter| letters.include?(letter) } &&
       # only select words that start with the start letters or are empty
-      (word[0..opts[:start_letters].length - 1] == opts[:start_letters] || opts[:start_letters].empty?) &&
+      (word[0..start_letters.length - 1] == start_letters || start_letters.empty?) &&
       # words that contain the key letter or word
-      (word.include?(opts[:key_letter_or_word]) || opts[:key_letter_or_word].empty?) &&
+      (word.include?(key_letter_or_word) || key_letter_or_word.empty?) &&
       # words that end with the end letters
-      (word[-opts[:end_letters].length..-1] == opts[:end_letters] || opts[:end_letters].empty?)
+      (word[-end_letters.length..-1] == end_letters || end_letters.empty?)
     end
-
-    puts "Remaining candidates (#{dict.length} words after filter) are now #{100.0 - percentage(dict.length, full_dict_length)}% shorter than full dictionary."
-  end
-
-  def find
-    puts "Possibilities for #{word_length} words are:\n"
-    pp @dict
   end
 
   private
@@ -48,21 +35,16 @@ class WordFinder
 end
 
 
-def spelling_bee_solver(letters, key_letter_or_word)
-  15.downto(4).each do |word_length|
-    WordFinder.new(letters:, word_length:, key_letter_or_word:).find
+def spelling_bee_solver(letters:, word_length:, key_letter_or_word:, start_letters:, end_letters:)
+  output = []
+  word_length.downto(4).each do |word_length|
+   output << WordFinder.new(letters:, word_length:, key_letter_or_word:, start_letters:, end_letters:).find
   end
+  output.flatten
 end
 
-spelling_bee_solver('tyranic', 'c')
+pp spelling_bee_solver(letters: 'confirm', key_letter_or_word: 'o', word_length: 6, start_letters: 'c', end_letters: 'n')
 
-# WordFinder.new(
-#   key_letter_or_word: 'g',
-#   start_letters: '',
-#   end_letters: '',
-#   letters: 'cogenta',
-#   word_length: 6
-# ).find
 
 
 
