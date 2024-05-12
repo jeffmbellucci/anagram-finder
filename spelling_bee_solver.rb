2346efcf780792e764e38bab41c44db87ad06373
Jeff <jeffmbellucci@gmail.com>
2024-05-12 06:22:09 -0700

    order spelling be solutions by word frequency

--- spelling_bee_solver.rb
+++ spelling_bee_solver.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class WordFinder
   attr_reader :letters, :dict, :candidates, :word_length, :key_letter_or_word, :start_letters, :end_letters
 
@@ -8,10 +10,8 @@ class WordFinder
     @key_letter_or_word = key_letter_or_word
     @start_letters = start_letters
     @end_letters = end_letters
-
     # puts "\nFull dictionary length: #{full_dict_length} words."
     # puts "Using '#{opts[:start_letters]}' to start and containing '#{opts[:key_letter_or_word]}' using only '#{letters.split('').join(',')}'."
-
   end
 
   def find
@@ -28,14 +28,6 @@ class WordFinder
     end
   end
 
-def wordle_possible
-  output = find.select do |word|
-    key_letter_or_word.chars.all? { |letter| word.include?(letter) } &&
-    word.length == 5
-  end
-  output
-end
-
   # Return only the words that contain all the letter
   def anagrams
     dict.select { |word| word.chars.sort.join == letters.chars.sort.join } - [letters]
@@ -48,12 +40,23 @@ end
   end
 end
 
-def spelling_bee_solver(letters:, word_length: 15, key_letter_or_word:, start_letters:, end_letters:)
- output = []
+# Main method to solve the spelling bee puzzle, pass false as the last argument to return only the words in the frequency hash
+def spelling_bee_solver(letters:, word_length: 15, key_letter_or_word:, start_letters:, end_letters:, all_words: true)
+  output = []
+  # open word_frequency.csv and return as a hash with the words as keys and the frequency as integer values
+  frequency = CSV.read('./word_frequency.csv').to_h.transform_values(&:to_i)
+  # iterate through the word lengths from the word_length to 4
   word_length.downto(4).each do |word_length|
    output << WordFinder.new(letters:, word_length:, key_letter_or_word:, start_letters:, end_letters:).find
   end
-  output.flatten
+  # order the words by the frequency of the word in the English language or infinite if not found
+  # only select words that are in the frequency hash
+  entire_output = output.flatten.sort_by { |word| frequency[word] || 0 }.reverse
+  limited_output = output.flatten.select { |word| frequency[word] }.sort_by { |word| frequency[word] }.reverse
+  output = all_words ? entire_output : limited_output
+  output.each do |word|
+    puts "#{word} - #{frequency[word]}"
+  end
 end
 
 # not currently working, but it was at one point, will need to check git history
@@ -65,8 +68,11 @@ def all_panagrams(letters:, word_length:, key_letter_or_word:, start_letters:, e
   output
 end
 
-#pp WordFinder.new(letters: 'laster' , word_length: '', key_letter_or_word: '', start_letters: '', end_letters: '').anagrams
 
-pp spelling_bee_solver(letters: 'bloming', key_letter_or_word: 'b', word_length: 15, start_letters: '', end_letters: '')
 
-pp WordFinder.new(letters: 'aroqwyupfghjkzxvm', word_length: 5, key_letter_or_word: 'oar', start_letters: '', end_letters: '').wordle_possible
+#pp WordFinder.new(letters: 'playome' , word_length: '', key_letter_or_word: '', start_letters: '', end_letters: '').anagrams
+
+#spelling_bee_solver(letters: 'playome', key_letter_or_word: 'a', word_length: 15, start_letters: '', end_letters: '')
+
+#pp WordFinder.new(letters: 'qwertyuodfghjklzxvm', word_length: 5, key_letter_or_word: '', start_letters: '', end_letters: '').find.select { |word| word.include?('r') && word.include?('e') && word.include?('t') && word[1] != 'r' && word[2] != 'r' && word[3] == 'e'  && word[4] != 'e' && word[4] != 't'}
+
